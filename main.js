0f5504e1968284ab628bdec4966dfb0b0aaf3bc0
lola martinhao <lola.martinhao@gmail.com>
2022-12-27 16:53:02 -0300

    improving javascript code

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let i = 0;
-
 function tocaSom(idElementoAudio) {
   document.querySelector(idElementoAudio).play();
 }
 
-while (i < listaDeTeclas.length) {
-  const tecla = listaDeTeclas[i];
+for (let index = 0; index < listaDeTeclas.length; index++) {
+  const tecla = listaDeTeclas[index];
   const som = tecla.classList[1];
   const idAudio = `#som_${som}`;
 
   tecla.onclick = function () {
     tocaSom(idAudio);
   }
-  i += 1;
+
 }
